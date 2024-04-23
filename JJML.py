78d9a5801e28f4788407c58af944a3be4b875bae
Drayko <draykom@gmail.com>
2024-04-23 14:17:00 +1000

    Created add_position_if_not_exists()

--- JJML.py
+++ JJML.py
@@ -1,6 +1,11 @@
 import graphviz
 import regex as re
 
+# GLOBALS
+i = 0
+position_dict = {}
+position_nodes = set()
+step_nodes = set()
 dot = graphviz.Digraph(comment='Jiu Jitsu')
 
 MAJOR = {
@@ -8,10 +13,22 @@ MAJOR = {
     'fontcolor':'white',
     'style':'filled',
     }
+MAJOR_TOP = "#27AE60"
+MAJOR_BOTTOM = "#E74C3C"
+MAJOR_NEUTRAL = "#F1C40F"
 
 MINOR = {
     'shape':'hexagon',
     'style':'filled',
+}
+MINOR_TOP = "#58D68D"
+MINOR_BOTTOM = "#EC7063"
+MINOR_NEUTRAL = "#F4D03F"
+
+symbol_to_key = {
+    '^': "TOP_",
+    '!': "BOT_",
+    '~': "NEU_"
 }
 
 FNAME = "sample.jjml"
@@ -20,15 +37,38 @@ with open(FNAME) as f:
     for line in f:
         lines.append(line)
 
-i = 0
-nodes = {}
+
 def position_parsing(lines, type):
     global i
     while lines[i] != "}\n":
+        position = lines[i].strip().split(": ")
+        position_dict[position[0]] = (position[1], type)
         i += 1
 
+def add_position_if_not_exists(style, name, tip):
+    if name not in position_nodes:
+        if position_dict[name][1] == "major":
+            style_dict = MAJOR
+            style_dict['color'] = MAJOR_TOP if style == '^' else MAJOR_BOTTOM if style == '!'else MAJOR_NEUTRAL
+        elif position_dict[name][1] == "minor":
+            style_dict = MINOR
+            style_dict['color'] = MINOR_TOP if style == '^' else MINOR_BOTTOM if style == '!'else MINOR_NEUTRAL
+        dot.node(f"{symbol_to_key[style]}{name}", position_dict[name][0] , style_dict, tooltip= tip)
+        position_nodes.add(name)
+
+def add_step_if_not_exists(style, name, tip):
+    if name not in position_nodes:
+        if position_dict[name][1] == "major":
+            style_dict = MAJOR
+            style_dict['color'] = MAJOR_TOP if style == '^' else MAJOR_BOTTOM if style == '!'else MAJOR_NEUTRAL
+        elif position_dict[name][1] == "minor":
+            style_dict = MINOR
+            style_dict['color'] = MINOR_TOP if style == '^' else MINOR_BOTTOM if style == '!'else MINOR_NEUTRAL
+        dot.node(f"{symbol_to_key[style]}{name}", position_dict[name][0], style_dict)
+        position_nodes.add(name)
+
 def tech_regex(line):
-    style_regex = r"([\^\-!#*?$])"
+    style_regex = r"([\^!~#*?$])"
     name_regex = r"((?:[a-z]+)|(?:\[[a-z ]+\]))"
     tip_regex = r"(?:{([a-z,;'\"\-\n<> ]+)})?"
     return re.findall(f"{style_regex}{name_regex}{tip_regex} -> {style_regex}{name_regex}{tip_regex}", line, overlapped=True, flags=re.IGNORECASE)
@@ -41,7 +81,12 @@ def tech_parsing(lines):
     while (lines[i] != "}\n") and (lines[i] != "}"):
         chain = tech_regex(lines[i])
         for step in chain:
-            print(step)
+            style_a, name_a, tip_a, style_b, name_b, tip_b = step
+            # Check if named
+            if style_a in ['^', '!', '~']:
+                add_position_if_not_exists(style_a, name_a, tip_a)
+            elif style_a in ['#', '*', '?', '$']:
+                pass
         i += 1
 
 while i < len(lines):
@@ -58,5 +103,5 @@ while i < len(lines):
                 tech_parsing(lines)
     i += 1
 
-print(nodes)
+print(position_nodes)
 print(dot.source)
