1e1bab3d4f01d8dc90a0b25e4543353aab6f0ecf
Drayko <draykom@gmail.com>
2024-04-23 13:40:40 +1000

    Added tech regex

--- JJML.py
+++ JJML.py
@@ -1,5 +1,5 @@
 import graphviz
-import re
+import regex as re
 
 dot = graphviz.Digraph(comment='Jiu Jitsu')
 
@@ -27,13 +27,21 @@ def position_parsing(lines, type):
     while lines[i] != "}\n":
         i += 1
 
+def tech_regex(line):
+    style_regex = r"([\^\-!#*?$])"
+    name_regex = r"((?:[a-z]+)|(?:\[[a-z ]+\]))"
+    tip_regex = r"(?:{([a-z,;'\"\-\n<> ]+)})?"
+    return re.findall(f"{style_regex}{name_regex}{tip_regex} -> {style_regex}{name_regex}{tip_regex}", line, overlapped=True, flags=re.IGNORECASE)
+
 def tech_parsing(lines):
     global i
     m = re.match(r"tech \(([a-z_]+)\) {", lines[i])
     print(m.group(1))
     i += 1
     while (lines[i] != "}\n") and (lines[i] != "}"):
-        pass
+        chain = tech_regex(lines[i])
+        for step in chain:
+            print(step)
         i += 1
 
 while i < len(lines):
